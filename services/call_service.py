4b605b2f2a296a46f5b4e911c0c73a7f6c0e6db8
mubashir <mubashirnaeemj@gmail.com>
2026-04-13 17:01:05 +0500

    hardcoding ph id

--- services/call_service.py
+++ services/call_service.py
@@ -9,12 +9,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def make_call(phone_id, to_number, address, agent_id):
-    phone_id = "phnum_6301km04ej3rex6aqyexwmsn4f63"
+    phone_id = "phnum_0001khx5hqh7fbkryh4ymgsnwns5"
     logger.info(f"Making call | to: {to_number}, from_id: {phone_id}, address: {address}")
     
     payload = {
         "agent_id": agent_id,
-        "agent_phone_number_id": "phnum_6301km04ej3rex6aqyexwmsn4f63",
+        "agent_phone_number_id": phone_id,
         "to_number": to_number,
         "conversation_initiation_client_data": {
             "dynamic_variables": {
